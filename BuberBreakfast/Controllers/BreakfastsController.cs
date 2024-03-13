006a1f6f09186bc3caa2a4a6bce421fdc89501ce
Mukul Khanna <mukulkhanna.96@gmail.com>
2024-03-13 11:41:31 +0530

    API requests

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -4,27 +4,29 @@ using Microsoft.AspNetCore.Mvc;
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
+//[Route("breakfasts")]
+[Route("[controller]")]
 public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfasts")]
+    [HttpPost]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
         return Ok(id);
     }
 
-    [HttpPut("/breakfasts/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfstRequest request)
     {
         return Ok(request);
     }
 
-    [HttpDelete("/breakfasts/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
         return Ok(id);
